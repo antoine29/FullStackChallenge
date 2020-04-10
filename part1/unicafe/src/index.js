09c89093c14ee4ee36f1c8c44be0fd6090b705c3
Antoine <anthonyfg9@gmail.com>
2020-04-10 19:12:47 -0400

    p1,d,1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const goodText = 'good'
+const neutralText = 'neutral'
+const badText = 'bad'
+const allText = 'all'
+const averageText = 'average'
+const positivePercText = 'positive'
+
 const Title = ({title}) => <h2>{title}</h2>
 const Button = ({onClickFunc, text}) => <button onClick={onClickFunc}>{text}</button>
 const Result = ({text, count}) => <p>{text} {count}</p>
+const Statistics = ({good, neutral, bad}) => {
+  const getAll = () => good+neutral+bad
+  const getAverage = () => (good + (bad*-1))/getAll()
+  const getPositivePercentage = () => (good/getAll()) * 100 
+
+  return(
+    <>
+      <Result text={goodText} count={good} />
+      <Result text={neutralText} count={neutral} />
+      <Result text={badText} count={bad} />
+      <Result text={allText} count={getAll()} />
+      <Result text={averageText} count={getAverage()} />
+      <Result text={positivePercText} count={getPositivePercentage()+' %'} />
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodText = 'good'
-  const neutralText = 'neutral'
-  const badText = 'bad'
-  const allText = 'all'
-  const averageText = 'average'
-  const positivePercText = 'positive'
 
   const printFunction = (text) => () => console.log(text)
 
@@ -29,10 +45,6 @@ const App = () => {
     }
   }
 
-  const getAll = () => good+neutral+bad
-  const getAverage = () => (good + (bad*-1))/getAll()
-  const getPositivePercentage = () => (good/getAll()) * 100 
-
   return (
     <>
       <Title title='Give feedback' />
@@ -41,12 +53,7 @@ const App = () => {
       <Button onClickFunc={handleButtonClick(neutralText)} text={neutralText} />
       <Button onClickFunc={handleButtonClick(badText)} text={badText} />
 
-      <Result text={goodText} count={good} />
-      <Result text={neutralText} count={neutral} />
-      <Result text={badText} count={bad} />
-      <Result text={allText} count={getAll()} />
-      <Result text={averageText} count={getAverage()} />
-      <Result text={positivePercText} count={getPositivePercentage()+' %'} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
