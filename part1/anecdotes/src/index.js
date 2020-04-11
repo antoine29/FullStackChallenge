49586433a53f1ac0e848b4fad41404c6245d7e2b
Antoine <anthonyfg9@gmail.com>
2020-04-10 21:38:12 -0400

    p1,d,1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -14,11 +14,27 @@ const getRandomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+const MostVoted = ({votes}) => {
+  const mostVotedAnecdoteIndex = 
+    votes.reduce((higherVote, vote, currentIndex) =>
+      vote > votes[higherVote] ? currentIndex : higherVote, 0)
+  
+  return ( mostVotedAnecdoteIndex > 0 ? 
+    <>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[mostVotedAnecdoteIndex]}</p>
+      <p>has {votes[mostVotedAnecdoteIndex]} votes</p>
+    </> :
+    <></>
+  )    
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, incrementVotes] = useState(new Array(anecdotes.length).fill(0))
   
   const setRandomAnecdote = () => setSelected(getRandomInteger(0, anecdotes.length))
+  
   const setVote = () => {
     const incrementedVotes = [...votes]
     incrementedVotes[selected] += 1;
@@ -28,15 +44,16 @@ const App = (props) => {
 
   return (
     <>
+      <h2>Anecdote of the day</h2>
       <div>
         {props.anecdotes[selected]}
       </div>
       <button onClick={setVote}> vote </button>
       <button onClick={setRandomAnecdote}> next anecdote </button>
+      <MostVoted votes={votes} />
     </>
   )
 }
-
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
