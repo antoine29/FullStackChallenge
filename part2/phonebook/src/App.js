067de85766de88f7f9d6e5435399bfb8afe8661b
Antoine <anthonyfg9@gmail.com>
2020-04-11 21:25:27 -0400

    adding part2,b,2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNewName = (event) => {
+    event.preventDefault()
+    console.log('adding ', newName)
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
