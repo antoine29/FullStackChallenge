dc90d06daadee33fb8cac9615590a5c7ce89363b
Antoine <anthonyfg9@gmail.com>
2020-05-23 20:47:12 -0400

    part3, a, 3.3-6

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
 	{ 
 		name: "Arto Hellas", 
@@ -37,7 +39,56 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(p => p.id === id)
+	res = person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(p => p.id !== id)
+	res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+	if (!req.body.name || !req.body.number)
+		return res
+			.status(400)
+			.json({
+				error: "missing content"
+			})
+
+	if (personNameValidation(req.body.name))
+		return res
+			.status(400)
+			.json({
+				error: "name must be unique"
+			})			
+
+	const newPerson = {
+		name: req.body.name,
+		number: req.body.number,
+		id: getValidPersonId()
+	}
+	console.log("Adding ", newPerson)
+	persons = persons.concat(newPerson)
+	res.json(newPerson)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const getValidPersonId = () => {
+	let newId = undefined
+	do {
+		newId = Math.floor(Math.random()*1000)
+		console.log("in")
+	} while (persons.find(p => p.id === newId));
+
+	return newId
+}
+
+const personNameValidation = (name) => persons.find(p => p.name === name)
