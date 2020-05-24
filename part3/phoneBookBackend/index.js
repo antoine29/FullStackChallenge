60ca82ec4409949eee43fd7b1e60e3d8cd82d2c9
Antoine <anthonyfg9@gmail.com>
2020-05-24 19:09:04 -0400

    part3, a, 3.9-10

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', req => req.method === "POST" ? JSON.stringify(req.body) : "")
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let persons = [
 	{ 
@@ -84,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
