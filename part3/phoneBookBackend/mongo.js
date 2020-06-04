7ec02d44b7b580a1345396586ae8db35dab2a4f6
Antoine <anthonyfg9@gmail.com>
2020-06-03 20:35:23 -0400

    part3, c, 3.12

---
+++ part3/phoneBookBackend/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the arguments as: "\$ node mongo.js <password> <name> <phone>\" in order to add a new contact or "\$ node mongo.js <password>\" in order to list the contacts.')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url =
+  `mongodb+srv://user:${password}@cluster0-y2l8w.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+	number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+switch (process.argv.length){
+	case 5: {
+		const newPerson = new Person({
+			name: process.argv[3],
+			number: process.argv[4]
+		})
+		
+		newPerson.save().then(result => {
+			console.log('new contact saved!')
+			mongoose.connection.close()
+		})
+
+		break
+	}
+
+	case 3: {
+		Person.find({}).then(result => {
+			console.log("phonebook:")
+			result.forEach(person => {
+		    console.log(person.name, '\t', person.number)
+		  })
+		  mongoose.connection.close()
+		})
+
+		break
+	}
+
+	default: {
+		console.log('Please provide the arguments as: "\$ node mongo.js <password> <name> <phone>\" in order to add a new contact or "\$ node mongo.js <password>\" in order to list the contacts.')
+		process.exit(1)		
+	}
+}
