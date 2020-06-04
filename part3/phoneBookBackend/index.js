eece81f26af70f7ef8143b580fc3abfc7c9cd201
Antoine <anthonyfg9@gmail.com>
2020-06-04 11:48:43 -0400

    part3, c, 3.13-14

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
+
 const app = express()
 
 morgan.token('body', req => req.method === "POST" ? JSON.stringify(req.body) : "")
@@ -10,75 +13,71 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
-let persons = [
-	{ 
-		name: "Arto Hellas", 
-		number: "040-123456",
-		id: 1
-	},
-	{ 
-		name: "Ada Lovelace", 
-		number: "39-44-5323523",
-		id: 2
-	},
-	{ 
-		name: "Dan Abramov", 
-		number: "12-43-234345",
-		id: 3
-	},
-	{ 
-		name: "Mary Poppendieck", 
-		number: "39-23-6423122",
-		id: 4
-	}
-]
-
 app.get('/', (req, res) => {
 	res.send('<h1>Hello PhoneBook!</h1>')
 })
 
 app.get('/info', (req, res) => {
-	res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+	Person
+		.countDocuments({})
+		.then(count => res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`))
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+	Person
+		.find({})
+		.then(persons => {
+    	res.json(persons)
+  	})
 })
 
 app.get('/api/persons/:id', (req, res) => {
 	const id = Number(req.params.id)
-	const person = persons.find(p => p.id === id)
-	res = person ? res.json(person) : res.status(404).end()
+	Person
+		.findOne({id: id})
+		.then(person => { person ? res.json(person) : res.status(404).end()})
 })
 
 app.delete('/api/persons/:id', (req, res) => {
 	const id = Number(req.params.id)
-	persons = persons.filter(p => p.id !== id)
-	res.status(204).end()
+	Person
+		.deleteOne({id: id})
+		.then(_ => res.status(204).end())
 })
 
 app.post('/api/persons', (req, res) => {
-	if (!req.body.name || !req.body.number)
+	const body = req.body
+	if (!body.name || !body.number)
 		return res
 			.status(400)
 			.json({
 				error: "missing content"
 			})
-
-	if (personNameValidation(req.body.name))
-		return res
-			.status(400)
-			.json({
-				error: "name must be unique"
-			})			
-
-	const newPerson = {
-		name: req.body.name,
-		number: req.body.number,
-		id: getValidPersonId()
-	}
-	persons = persons.concat(newPerson)
-	res.json(newPerson)
+	
+	personNameValidation(body.name)
+		.then(isValidName => {			
+			if (isValidName){
+				getValidPersonId()
+					.then(validId => {
+						console.log("valid id: ", validId)
+						const newPerson = new Person({
+							name: body.name,
+							number: body.number,
+							id: validId
+						})
+					
+						newPerson
+							.save()
+							.then(savedPerson => { res.json(savedPerson) })
+					})
+			}
+			else 
+				return res
+					.status(400)
+					.json({
+						error: "name must be unique"
+					})		
+		})
 })
 
 const unknownEndpoint = (request, response) => {
@@ -87,19 +86,22 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-const getValidPersonId = () => {
-	let newId = undefined
-	do {
-		newId = Math.floor(Math.random()*1000)
-		console.log("in")
-	} while (persons.find(p => p.id === newId));
+const getValidPersonId = () =>
+	Person
+	.find({})
+	.then(persons => {
+		let newId = undefined
+		do {
+			newId = Math.floor(Math.random()*1000)
+		} while (persons.find(p => p.id === newId))
 
-	return newId
-}
+		return newId
+	})
 
-const personNameValidation = (name) => persons.find(p => p.name === name)
+const personNameValidation = name =>
+	Person.find({}).then(persons => !persons.find(p => p.name === name))
