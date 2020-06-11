a52fd5ff4f60a76b96b4ab46eed9715e8a471eae
Antoine <anthonyfg9@gmail.com>
2020-06-10 22:18:31 -0400

    part4, a, 4.6-7

--- part4/blogList/tests/test.js
+++ part4/blogList/tests/test.js
@@ -12,13 +12,13 @@ const blogs1 = [
 const blogs2 = [
 	{
 		title: 'title0',
-		author: 'author0',
+		author: 'author1',
 		likes: 2
 	},
 	{
 		title: 'title1',
-		author: 'author1',
-		likes: 3
+		author: 'author0',
+		likes: 2
 	}
 ]
 const blogs3 = [
@@ -29,7 +29,7 @@ const blogs3 = [
 	},
 	{
 		title: 'title1',
-		author: 'author1',
+		author: 'author0',
 		likes: 3
 	},
 	{
@@ -49,7 +49,7 @@ describe('dummy', () => {
 describe('total lkes', () => {
 	test('when blog list has five total likes, ', () => {
 		const result = listHelper.totalLikes(blogs2)
-		expect(result).toBe(5)
+		expect(result).toBe(4)
 	})
 
 	test('when blog list is empty, ', () => {
@@ -82,5 +82,49 @@ describe('favorite blog', () => {
 	test('when there is more than one favorite blog, returns the first, ', () => {
 		const result = listHelper.favoriteBlog(blogs3)
 		expect(result).toEqual(blogs3[1])
+	})
+})
+
+describe('mostBlogs', () => {
+	test('when the blogest author has two blogs, ', () => {
+		const result = listHelper.mostBlogs(blogs3)
+		expect(result).toEqual({ author: 'author0', blogs: 2 })
+	})
+
+	test('when there is only one blog, ', () => {
+		const result = listHelper.mostBlogs(blogs1)
+		expect(result).toEqual({ author: 'author0', blogs: 1 })
+	})
+
+	test('when there is more than one blogued authors, returns the first ', () => {
+		const result = listHelper.mostBlogs(blogs2)
+		expect(result).toEqual({ author: 'author1', blogs: 1 })
+	})
+
+	test('when pass an empty array', () => {
+		const result = listHelper.mostBlogs(blogs0)
+		expect(result).toEqual({})
+	})
+})
+
+describe('mostLikes', () => {
+	test('when the most liked author has 5 likes, ', () => {
+		const result = listHelper.mostLikes(blogs3)
+		expect(result).toEqual({ author: 'author0', likes: 5 })
+	})
+
+	test('when there is only one blog, ', () => {
+		const result = listHelper.mostLikes(blogs1)
+		expect(result).toEqual({ author: 'author0', likes: 2 })
+	})
+
+	test('when there is more than most liked authors, returns the first ', () => {
+		const result = listHelper.mostLikes(blogs2)
+		expect(result).toEqual({ author: 'author1', likes: 2 })
+	})
+
+	test('when pass an empty array', () => {
+		const result = listHelper.mostLikes(blogs0)
+		expect(result).toEqual({})
 	})
 })
--- part4/blogList/utils/list_helpers.js
+++ part4/blogList/utils/list_helpers.js
@@ -6,8 +6,35 @@ const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 const favoriteBlog = blogs =>
 	blogs.reduce((mostLikedBlog, blog) => blog.likes > mostLikedBlog.likes? blog : mostLikedBlog, blogs[0])
 
+const mostBlogs = blogs => {
+	if (blogs.length === 0) return {}
+	const authorGrouped = _.reduce(blogs, (result, blog) => {
+		(result[blog.author] || (result[blog.author] = [])).push(blog)
+		return result
+	}, {})
+
+	const values = _.values(authorGrouped)
+	return _.reduce(values, (author, group) =>
+		group.length > author.blogs ?
+			{ author: group[0].author, blogs: group.length } : author, { author: values[0][0].author, blogs: values[0].length })
+}
+
+const mostLikes = blogs => {
+	if (blogs.length === 0) return {}
+	const likesCounted = _.reduce(blogs, (result, blog) => {
+		result[blog.author] !== undefined ?
+			result[blog.author] += blog.likes : result[blog.author] = blog.likes
+		return result
+	}, {})
+
+	return _.reduce(_.values(likesCounted), (max, author, index) =>
+		author > max.likes ? { author: _.keys(likesCounted)[index], likes: author } : max, { author: 'undefined', likes: 0 })
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
-	favoriteBlog
+	favoriteBlog,
+	mostBlogs,
+	mostLikes
 }
