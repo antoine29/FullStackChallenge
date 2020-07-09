adc72670e39c439f588d5a455877cd5ef5a2c3e9
Antoine <anthonyfg9@gmail.com>
2020-07-08 21:51:41 -0400

    part5, d, 5.17-22 completing e2e tests

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,12 +1,18 @@
 describe('Blog app', function () {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    const user = {
+    const user0 = {
       name: 'user0',
       username: 'user0',
       password: 'password0'
     }
-    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    const user1 = {
+      name: 'user1',
+      username: 'user1',
+      password: 'password1'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user0)
+    cy.request('POST', 'http://localhost:3001/api/users/', user1)
     cy.visit('http://localhost:3000')
   })
 
@@ -14,23 +20,30 @@ describe('Blog app', function () {
     cy.contains('Blog List')
   })
 
-  it('login fails with wrong password', function() {
+  it('login form is shown', function() {
     cy.contains('log in').click()
-    cy.get('#username').type('userx')
-    cy.get('#password').type('passwordx')
-    cy.get('#loginButton').click()
-    cy.get('.error').contains('Wrong credentials')
-    cy.get('html').should('not.contain', 'userx logged-in')
+    cy.contains('username')
+    cy.contains('password')
   })
 
-  it('login form can be opened', function() {
-    cy.contains('log in').click()
-    cy.get('#username').type('user0')
-    cy.get('#password').type('password0')
-    cy.get('#loginButton').click()
-    cy.contains('user0 logged-in')
-  })
+  describe('LogIn', function() {
+    it('login fails with wrong password', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('userx')
+      cy.get('#password').type('passwordx')
+      cy.get('#loginButton').click()
+      cy.get('.error').contains('Wrong credentials')
+      cy.get('html').should('not.contain', 'userx logged-in')
+    })
 
+    it('login works with valid credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('user0')
+      cy.get('#password').type('password0')
+      cy.get('#loginButton').click()
+      cy.contains('user0 logged-in')
+    })
+  })
 
   describe('when logged in', function() {
     beforeEach(function() {
@@ -46,13 +59,26 @@ describe('Blog app', function () {
       cy.contains('titleTest')
     })
 
-    describe('and a blog exists', function () {
+    describe('and when a blog exists', function () {
       it('a created blog can be liked', function() {
         cy.createBlog({ author: 'author0', title: 'title0', url: 'url0' })
         cy.contains('title0')
         cy.get('.viewButton').click()
         cy.get('.likeButton').click()
         cy.contains('likes: 1')
+      })
+
+      it('a blog can be deleted by its creator', function() {
+        cy.createBlog({ author: 'author0', title: 'title0', url: 'url0' })
+        cy.contains('title0')
+        cy.get('.viewButton').click()
+        cy.contains('delete')
+      })
+
+      it('a blog can\'t be deleted by another user', function() {
+        cy.createBlog({ author: 'author0', title: 'title0', url: 'url0' }).then
+        cy.login({ username: 'user1', password: 'password1' })
+        cy.get('html').should('not.contain', 'delete')
       })
     })
   })
