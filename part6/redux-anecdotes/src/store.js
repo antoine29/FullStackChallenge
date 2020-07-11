52f371451b95e05209a5456e007c92aaae1f2ffd
Antoine <anthonyfg9@gmail.com>
2020-07-10 20:42:00 -0400

    setting up reduxDevTools

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer, composeWithDevTools())
+console.log(store.getState())
+
+export default store
