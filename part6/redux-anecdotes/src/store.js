ab0f6eac6a7e6b76bab7e4fed958f5d14b85c997
Antoine <anthonyfg9@gmail.com>
2020-07-11 21:15:18 -0400

    setting up redux-thunk

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
@@ -10,7 +11,10 @@ const reducer = combineReducers({
   notification: notificationReducer
 })
 
-const store = createStore(reducer, composeWithDevTools())
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 console.log('initial store:', store.getState())
 
 export default store
