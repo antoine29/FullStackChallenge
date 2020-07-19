d1c7bf57926848d3d12fbc12e61806cae8fda240
Antoine <anthonyfg9@gmail.com>
2020-07-18 21:22:27 -0400

    part7, a, 4-6

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from "react-router-dom"
+import { useField } from '../customHooks/'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
 
     const history = useHistory();
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
 
-        props.setNotification(`a new anecdote '${content}' created!`)
+        props.setNotification(`a new anecdote '${content.value}' created!`)
         history.push('/')
         setTimeout(() => props.setNotification(''), 10000)
+    }
+
+    const handleReset = () => {
+        content.setValue('')
+        author.setValue('')
+        info.setValue('')
+    }
+
+    const filterInputProps = fieldHook => {
+        const {setValue, ...props} = fieldHook
+        return props
     }
 
     return (
@@ -27,21 +39,21 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+                    <input {...filterInputProps(content)}/>
                 </div>
                 <div>
                     author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input {...filterInputProps(author)}/>
                 </div>
                 <div>
                     url for more info
-            <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+                    <input {...filterInputProps(info)} />
                 </div>
                 <button>create</button>
+                <button type='button' onClick={handleReset}>reset</button>
             </form>
         </div>
     )
-
 }
 
 export default CreateNew
