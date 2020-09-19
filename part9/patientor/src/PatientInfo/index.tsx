1f020ed83128fc57a395c43720484d123c09e52c
Antoine <anthonyfg9@gmail.com>
2020-09-18 22:11:22 -0400

    part9, d 16-18

---
+++ part9/patientor/src/PatientInfo/index.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+import axios from "axios";
+import { Table, Icon } from "semantic-ui-react";
+
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue, updatePatient } from "../state";
+
+const GetPatientInfo = async (patientId: string) => {
+  try{
+    const { data: patient } = await axios.get<Patient>(`${apiBaseUrl}/patients/${patientId}`);
+    return patient;
+  }
+  catch(error){
+    console.log(`Error getting info of patient: ${patientId}`);
+  }
+}
+
+const PatientGender = (patient: Patient): string =>
+  patient.gender === 'male'?
+    'male' : patient.gender === 'female' ?
+      'female' : 'other';
+
+const PatientInfo: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id: patientId } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | undefined>(undefined);
+
+  useEffect(() => {
+    const localPatient = patients[patientId];
+    if(localPatient && (!localPatient.entries || !localPatient.ssn)){
+      console.log(`getting info of patient: ${patientId}`);
+      const fetchPatientInfo = async () => {
+        const patient = await GetPatientInfo(patientId);
+        console.log('patient fetched:', patient);
+        // dispatch({ type: "UPDATE_PATIENT", payload: patient });
+        dispatch(updatePatient(patient));
+        setPatient(patient);
+      }
+      
+      fetchPatientInfo();
+    }
+
+    setPatient(localPatient);
+  }, [patientId]);
+
+  return patient ? (
+    <div className="App">
+      <Table celled>
+        <Table.Body>
+          <Table.Row key={patient.id}>
+            <Table.Cell>
+              <h2>
+                {patient.name + " "}
+                <Icon
+                  fitted
+                  name={PatientGender(patient) === 'female' ? 'venus' :  PatientGender(patient) === 'male' ? 'mars' : 'venus mars' } />
+              </h2>
+            </Table.Cell>
+            <Table.Cell>ssn: {patient.ssn}</Table.Cell>
+            <Table.Cell>occupation: {patient.occupation}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
+  ) : null;
+};
+
+export default PatientInfo;
--- part9/patientor/src/state/reducer.ts
+++ part9/patientor/src/state/reducer.ts
@@ -9,6 +9,10 @@ export type Action =
   | {
       type: "ADD_PATIENT";
       payload: Patient;
+    }
+  | {
+      type: "UPDATE_PATIENT";
+      payload: Patient | undefined;
     };
 
 export const reducer = (state: State, action: Action): State => {
@@ -32,7 +36,34 @@ export const reducer = (state: State, action: Action): State => {
           [action.payload.id]: action.payload
         }
       };
+    case "UPDATE_PATIENT":{
+      if(action.payload){
+        const updatedPatients = { ...state.patients };
+        updatedPatients[action.payload.id] = action.payload;
+        return {
+          ...state,
+          patients: updatedPatients
+        };
+      }
+
+      return { ...state }
+    }
     default:
       return state;
   }
 };
+
+export const setPatients = (patients: Patient[]): Action => ({
+  type: "SET_PATIENT_LIST",
+  payload: patients
+});
+
+export const addPatient = (patient: Patient): Action => ({
+  type: "ADD_PATIENT",
+  payload: patient
+});
+
+export const updatePatient = (patient: Patient | undefined): Action => ({
+  type: "UPDATE_PATIENT",
+  payload: patient
+});
