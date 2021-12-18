91ca99d44d593e6c4f10626e500b86d9e9d09711
anthony <anthony@mail.com>
2021-12-18 19:48:45 -0400

    deleting unnecesary model fields

--- part4/blogList/models/Blog.js
+++ part4/blogList/models/Blog.js
@@ -9,18 +9,10 @@ const blogSchema = new mongoose.Schema({
 		minlength: 3,
 		required: true,
 	},
-	author: {
-		type: String,
-		minlength: 3,
-		required: true
-	},
-	url: {
-		type: String,
-		required: true,
-	},
 	likes: {
 		type: Number,
-		required: true
+		//required: true
+		default: 0
 	},
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
--- part4/blogList/models/User.js
+++ part4/blogList/models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-	name: String,
 	username: {
 		type: String,
 		required: true,
